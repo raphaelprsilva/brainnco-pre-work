1ef5d2587035f698ba0cf7ed12b6b4f1afa49fb4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-18 08:34:52 -0300

    Add exercise 04

--- desafios-pre-work-b-academy-main/dia1/src/main.js
+++ desafios-pre-work-b-academy-main/dia1/src/main.js
@@ -3,4 +3,14 @@ import './style.css'
 document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+`;
+
+const link = document.querySelector('[data-js="change-visibility"]');
+const div = document.querySelector('[data-js="app"]');
+
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  div.style.display === "none"
+    ? (div.style.display = "block")
+    : (div.style.display = "none");
+}, false);
