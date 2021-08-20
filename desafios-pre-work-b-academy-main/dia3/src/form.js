255068d7a25c19c876d0ec89d0f11800747637a4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-20 17:30:48 -0300

    Add exercise 01 complete

--- desafios-pre-work-b-academy-main/dia3/src/form.js
+++ desafios-pre-work-b-academy-main/dia3/src/form.js
@@ -1,26 +1,17 @@
 const form = document.querySelector('[data-js="form"]');
-const input = document.querySelector('[data-js="input"]');
 
-// input.style.textTransform = "capitalize";
 
 form.addEventListener('keyup', (event) => {
   const targetValue = event.target.value;
-  const splitedString = targetValue.split(' ');
+  const inputPhrase = targetValue.split(' ');
 
-  console.log('splitedString', splitedString);
+  const newPhrase = inputPhrase.map((word) => {
+    const wordsToExclude = ['de', 'do', 'da', 'das', 'dos'];
+    return wordsToExclude.includes(word.toLowerCase())
+      ? word.toLowerCase()
+      : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join(' ');
 
-  const newArray = splitedString.map((word) => {
-    if (word === 'da' || word === 'de' || word === 'do' || word === 'dos') {
-      return word;
-    }
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  });
 
-  console.log('newArray', newArray)
-
-  console.log('newString', newArray.join(' '))
-  input.value = newArray.join(' ');
-  // console.log(input.value);
+  event.target.value = newPhrase;
 })
-
-
