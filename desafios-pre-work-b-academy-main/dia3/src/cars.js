5a8336412d9f01d641a8dd691a8e5fffd2970160
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-21 22:17:14 -0300

    Add exercise 03 - Complete

---
+++ desafios-pre-work-b-academy-main/dia3/src/cars.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('[data-js="cars-registration"]');
+const table = document.querySelector('[data-js="table"]');
+
+console.log(form.image)
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const image = event.target.elements['image'].value;
+  const year = event.target.elements['year'].value;
+  const model = event.target.elements['model'].value;
+  const plate = event.target.elements['plate'].value;
+  const color = event.target.elements['color'].value;
+  const items = [image, year, model, plate, color];
+
+  const tr = document.createElement('tr');
+  items.forEach((item, index) => {
+    if(index === 0) {
+      const td = document.createElement('td');
+      const img = document.createElement('img');
+      img.src = item;
+      td.appendChild(img);
+      tr.appendChild(td);
+    } else {
+      const td = document.createElement('td');
+      td.textContent = item;
+      tr.appendChild(td);
+    }
+  });
+
+  table.appendChild(tr);
+  form.reset();
+  form.image.focus();
+})
