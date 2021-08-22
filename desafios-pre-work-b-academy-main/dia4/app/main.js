589beb6d4806c2fbc8f2582d5ccaac564c1f9986
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-22 12:50:17 -0300

    Day 4 - Add code that add new car

--- desafios-pre-work-b-academy-main/dia4/app/main.js
+++ desafios-pre-work-b-academy-main/dia4/app/main.js
@@ -1,1 +1,35 @@
-import './style.css'
+import './style.css';
+
+const form = document.querySelector('[data-js="cars-registration"]');
+const table = document.querySelector('[data-js="table"]');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const image = event.target.elements['image'].value;
+  const year = event.target.elements['year'].value;
+  const model = event.target.elements['model'].value;
+  const plate = event.target.elements['plate'].value;
+  const color = event.target.elements['color'].value;
+  const items = [image, year, model, plate, color];
+
+  const tr = document.createElement('tr');
+  items.forEach((item, index) => {
+    if(index === 0) {
+      const td = document.createElement('td');
+      const img = document.createElement('img');
+      img.src = item;
+      img.width = 100;
+      img.height = 100;
+      td.appendChild(img);
+      tr.appendChild(td);
+    } else {
+      const td = document.createElement('td');
+      td.textContent = item;
+      tr.appendChild(td);
+    }
+  });
+
+  table.appendChild(tr);
+  form.reset();
+  form.image.focus();
+})
